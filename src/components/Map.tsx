ae35c8896b7103ded7f93bdfef004047fa9e8984
dev-vming <dev.vming@gmail.com>
2025-04-12 20:25:05 +0900

    feat: 마커 커스텀인포 추가 및 컴포넌트 분리

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,6 @@
 /*global kakao*/
 import Script from "next/script";
-import * as stores from "@/data/store_data.json";
+import { Dispatch, SetStateAction } from "react";
 
 declare global {
   interface Window {
@@ -8,10 +8,14 @@ declare global {
   }
 }
 
+interface MapProps{
+  setMap: Dispatch<SetStateAction<any>>;
+}
+
 const DEFAULT_LAT = 37.496486063;
 const DEFAULT_LNG = 127.028361548;
 
-export default function Map() {
+export default function Map({setMap}:MapProps) {
   const loadKakaoMap = () => {
     // 카카오 맵 불러오기, 초기 좌표 설정
     window.kakao.maps.load(() => {
@@ -21,36 +25,7 @@ export default function Map() {
         level: 3,
       };
       const map = new window.kakao.maps.Map(mapContainer, mapOption);
-
-      stores?.["DATA"]?.map((store) => {
-        // store data 좌표 값으로 마커 좌표 설정
-        const markerPosition = new window.kakao.maps.LatLng(
-          store?.y_dnts,
-          store?.x_cnts
-        );
-
-        // store 업태 명 별 마커 이미지 설정
-        const imageSrc = store?.bizcnd_code_nm
-            ? `/images/markers/${store?.bizcnd_code_nm}.png`
-            : "images/markers/default.png",
-          imageSize = new window.kakao.maps.Size(40, 40),
-          imageOption = { offset: new window.kakao.maps.Point(27, 69) };
-
-        const markerImage = new window.kakao.maps.MarkerImage(
-          imageSrc,
-          imageSize,
-          imageOption
-        );
-
-        // 마커 생성
-        const marker = new window.kakao.maps.Marker({
-          position: markerPosition,
-          image: markerImage,
-        });
-
-        // 지도 위에 마커 표시
-        marker.setMap(map);
-      });
+      setMap(map);
     });
   };
   return (
