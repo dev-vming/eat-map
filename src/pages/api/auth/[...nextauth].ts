52871adbc2084515b19cd469bbf30fcd0ec2eab9
dev-vming <dev.vming@gmail.com>
2025-05-12 22:54:21 +0900

    feat: 구글 프로바이더 설정

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
+import GoogleProvider from 'next-auth/providers/google';
 
 
 const prisma = new PrismaClient();
@@ -8,8 +9,12 @@ const prisma = new PrismaClient();
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    })
   ],
 };
 
 export default NextAuth(authOptions);
+
