0638f66fe867da988484ea6e859f1b1bef6a45f7
dev-vming <dev.vming@gmail.com>
2025-12-01 22:57:13 +0900

    feat: next-auth signIn 사용하여 클릭 시 로그인 기능 추가 및 세션 로그인 정보 있을 시 홈으로 이동

--- src/pages/users/login.tsx
+++ src/pages/users/login.tsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+
 import { AiOutlineGoogle } from "react-icons/ai";
 import { SiNaver } from "react-icons/si";
 import { RiKakaoTalkFill } from "react-icons/ri";
 
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
 
 export default function LoginPage() {
+  const { status, data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === 'authenticated') {
+      router.replace('/');
+    }
+  },[router, status])
+
   return (
     <div className="flex flex-col justify-center px-6 lg:px-8 h-[60vh]">
       <div className="mx-auto w-full max-w-sm">
@@ -19,20 +33,32 @@ export default function LoginPage() {
       </div>
       <div className="mt-10 mx-auto w-full max-w-sm">
         <div className="flex flex-col gap-3">
-          <button type="button" className="flex gap-4 text-white bg-[#4285F3] hover:bg-[#4285F3]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center">
-            <AiOutlineGoogle className="w-6 h-6"/>
+          <button
+            type="button"
+            onClick={() => signIn("google")}
+            className="flex gap-4 text-white bg-[#4285F3] hover:bg-[#4285F3]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
+          >
+            <AiOutlineGoogle className="w-6 h-6" />
             Sign in with Google
           </button>
-          <button type="button" className="flex gap-6 text-white bg-[#2db400] hover:bg-[#2db400]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center">
-            <SiNaver className="w-4 h-4"/>
+          <button
+            type="button"
+            onClick={() => signIn("naver")}
+            className="flex gap-6 text-white bg-[#2db400] hover:bg-[#2db400]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
+          >
+            <SiNaver className="w-4 h-4" />
             Sign in with Naver
           </button>
-          <button type="button" className="flex gap-5 text-black bg-[#fef01b] hover:bg-[#fef01b]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center">
-            <RiKakaoTalkFill className="w-6 h-6"/>
+          <button
+            type="button"
+            // onClick={() => signIn("kakao")}
+            className="flex gap-5 text-black bg-[#fef01b] hover:bg-[#fef01b]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
+          >
+            <RiKakaoTalkFill className="w-6 h-6" />
             Sign in with Kakao
           </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
