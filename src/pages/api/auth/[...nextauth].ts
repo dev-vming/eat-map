8d9d47a4b3738cb2517766bd7e23ce391fbdfc1d
dev-vming <dev.vming@gmail.com>
2025-12-01 22:31:50 +0900

    feat: 네이버 로그인 연동

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,8 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
-import GoogleProvider from 'next-auth/providers/google';
-
+import GoogleProvider from "next-auth/providers/google";
+import NaverProvider from "next-auth/providers/naver";
 
 const prisma = new PrismaClient();
 
@@ -10,11 +10,22 @@ export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    })
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID || "",
+      clientSecret: process.env.NAVER_CLIENT_SECRET || "",
+      profile(profile) {
+        return {
+          id: profile.response.id,
+          name: profile.response.name || profile.response.nickname || null,
+          email: profile.response.email,
+          image: profile.response.profile_image,
+        };
+      },
+    }),
   ],
 };
 
 export default NextAuth(authOptions);
-
