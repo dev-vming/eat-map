6b3a43a50b101385680fb8a2a346df33a8f6bc45
dev-vming <dev.vming@gmail.com>
2025-12-05 02:34:45 +0900

    feat: recoil atom 파일 및 인터페이스 추가

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,18 +1,24 @@
 export interface StoreType {
-  id: number;
-  phone?: string | null;
-  address?: string | null;
-  lat?: string | null;
-  lng?: string | null;
-  name?: string | null;
-  category?: string | null;
-  storeType?: string | null;
-  foodCertifyName?: string | null;
+    id: number;
+    phone?: string | null;
+    address?: string | null;
+    lat?: string | null;
+    lng?: string | null;
+    name?: string | null;
+    category?: string | null;
+    storeType?: string | null;
+    foodCertifyName?: string | null;
 }
 
 export interface StoreApiResponse {
-  data: StoreType[];
-  totalPage?: number;
-  totalCount?: number;
-  page?: number;
+    data: StoreType[];
+    totalPage?: number;
+    totalCount?: number;
+    page?: number;
+}
+
+export interface LocationType {
+    lat: number;
+  lng: number;
+  zoom: number;
 }
