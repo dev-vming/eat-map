876fc49a97934415de9735bca53a37f72649ab55
dev-vming <dev.vming@gmail.com>
2025-12-18 03:00:37 +0900

    feat: 등록, 수정 문구 수정

--- src/pages/stores/new.tsx
+++ src/pages/stores/new.tsx
@@ -194,7 +194,7 @@ export default function StoreNewPage() {
                     type="submit"
                     className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                    제출하기
+                    등록하기
                 </button>
             </div>
         </form>
