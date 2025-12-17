46bddd1ae91d2f9871af012787ed06e4b56c9dec
dev-vming <dev.vming@gmail.com>
2025-12-18 02:55:44 +0900

    feat: 맛집 상세 페이지에 수정, 삭제 버튼 추가 및 수정 페이지 UI 작업

--- src/pages/stores/[id]/index.tsx
+++ src/pages/stores/[id]/index.tsx
@@ -5,105 +5,136 @@ import { StoreType } from "@/interface";
 import Loader from "@/components/Loader";
 import Map from "@/components/Map";
 import Marker from "@/components/Marker";
-
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 export default function StoreDetailPage() {
-  const router = useRouter();
-  const { id } = router.query;
+    const router = useRouter();
+    const { id } = router.query;
+    const { status } = useSession();
 
-  const FetchStore = async () => {
-    const { data } = await axios(`/api/stores?id=${id}`);
-    return data as StoreType;
-  };
+    const FetchStore = async () => {
+        const { data } = await axios(`/api/stores?id=${id}`);
+        return data as StoreType;
+    };
 
-  const {
-    data: store,
-    isFetching,
-    isError,
-    isSuccess,
-  } = useQuery(`store-${id}`, FetchStore, {
-    enabled: !!id,
-    refetchOnWindowFocus: false,
-  });
+    const {
+        data: store,
+        isFetching,
+        isError,
+        isSuccess,
+    } = useQuery(`store-${id}`, FetchStore, {
+        enabled: !!id,
+        refetchOnWindowFocus: false,
+    });
 
-  if (isError) {
+    if (isError) {
+        return (
+            <div className="w-full h-screen mx-auto pt-[10%] text-gray-500 text-center font-semibold">
+                다시 시도해주세요.
+            </div>
+        );
+    }
+
+    if (isFetching) {
+        return <Loader className="mt-[40%]" />;
+    }
+
     return (
-      <div className="w-full h-screen mx-auto pt-[10%] text-gray-500 text-center font-semibold">
-        다시 시도해주세요.
-      </div>
+        <>
+            <div className="max-w-5xl mx-auto px-4 py-8">
+                <div className="md:flex justify-between items-center py-4 md:py-0">
+                    <div className="px-4 sm:px-0">
+                        <h3 className="text-base/7 font-semibold text-gray-900">
+                            {store?.name}
+                        </h3>
+                        <p className="mt-1 max-w-2xl text-sm/6 text-gray-500">
+                            {store?.address}
+                        </p>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <Link
+                            className="underline hover:text-gray-400 text-sm"
+                            href={`/stores/${store?.id}/edit`}
+                        >
+                            수정
+                        </Link>
+                        <button
+                            type="button"
+                            className="underline hover:text-gray-400 text-sm"
+                        >
+                            삭제
+                        </button>
+                    </div>
+                </div>
+
+                <div className="mt-6 border-t border-gray-100">
+                    <dl className="divide-y divide-gray-100">
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm/6 font-medium text-gray-900">
+                                카테고리
+                            </dt>
+                            <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                {store?.category}
+                            </dd>
+                        </div>
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm/6 font-medium text-gray-900">
+                                주소
+                            </dt>
+                            <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                {store?.address}
+                            </dd>
+                        </div>
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm/6 font-medium text-gray-900">
+                                위도
+                            </dt>
+                            <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                {store?.lat}
+                            </dd>
+                        </div>
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm/6 font-medium text-gray-900">
+                                경도
+                            </dt>
+                            <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                {store?.lng}
+                            </dd>
+                        </div>
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm/6 font-medium text-gray-900">
+                                연락처
+                            </dt>
+                            <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                {store?.phone}
+                            </dd>
+                        </div>
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm/6 font-medium text-gray-900">
+                                식품인증구분
+                            </dt>
+                            <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                {store?.foodCertifyName}
+                            </dd>
+                        </div>
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm/6 font-medium text-gray-900">
+                                업종명
+                            </dt>
+                            <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                {store?.storeType}
+                            </dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
+            {isSuccess && (
+                <div className="overflow-hidden w-full mb-20 max-w-5xl mx-auto max-h-[600px]">
+                    <Map lat={store.lat} lng={store.lng} zoom={1} />
+                    <Marker store={store} />
+                </div>
+            )}
+        </>
     );
-  }
-
-  if (isFetching) {
-    return <Loader className="mt-[40%]" />;
-  }
-
-  return (
-    <>
-      <div className="max-w-5xl mx-auto px-4 py-8">
-        <div className="px-4 sm:px-0">
-          <h3 className="text-base/7 font-semibold text-gray-900">
-            {store?.name}
-          </h3>
-          <p className="mt-1 max-w-2xl text-sm/6 text-gray-500">
-            {store?.address}
-          </p>
-        </div>
-        <div className="mt-6 border-t border-gray-100">
-          <dl className="divide-y divide-gray-100">
-            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-              <dt className="text-sm/6 font-medium text-gray-900">카테고리</dt>
-              <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                {store?.category}
-              </dd>
-            </div>
-            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-              <dt className="text-sm/6 font-medium text-gray-900">주소</dt>
-              <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                {store?.address}
-              </dd>
-            </div>
-            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-              <dt className="text-sm/6 font-medium text-gray-900">위도</dt>
-              <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                {store?.lat}
-              </dd>
-            </div>
-            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-              <dt className="text-sm/6 font-medium text-gray-900">경도</dt>
-              <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                {store?.lng}
-              </dd>
-            </div>
-            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-              <dt className="text-sm/6 font-medium text-gray-900">연락처</dt>
-              <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                {store?.phone}
-              </dd>
-            </div>
-            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-              <dt className="text-sm/6 font-medium text-gray-900">
-                식품인증구분
-              </dt>
-              <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                {store?.foodCertifyName}
-              </dd>
-            </div>
-            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-              <dt className="text-sm/6 font-medium text-gray-900">업종명</dt>
-              <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                {store?.storeType}
-              </dd>
-            </div>
-          </dl>
-        </div>
-      </div>
-      {isSuccess && (
-        <div className="overflow-hidden w-full mb-20 max-w-5xl mx-auto max-h-[600px]">
-          <Map lat={store.lat} lng={store.lng} zoom={1}/>
-          <Marker store={store} />
-        </div>
-      )}
-    </>
-  );
 }
