94ff567b10d83efebc001f52ad820fe5491202ed
dev-vming <dev.vming@gmail.com>
2025-12-18 03:00:37 +0900

    feat: 등록, 수정 문구 수정

--- src/pages/stores/[id]/edit.tsx
+++ src/pages/stores/[id]/edit.tsx
@@ -82,7 +82,7 @@ export default function StoreEditPage() {
                         맛집 수정
                     </h2>
                     <p className="mt-1 text-sm/6 text-gray-600">
-                        아래 내용을 입력해서 맛집을 등록해주세요.
+                        아래 내용을 입력해서 맛집을 수정해주세요.
                     </p>
 
                     <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -236,7 +236,7 @@ export default function StoreEditPage() {
                     type="submit"
                     className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                    제출하기
+                    수정하기
                 </button>
             </div>
         </form>
