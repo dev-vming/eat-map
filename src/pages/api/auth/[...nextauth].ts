6eea704380d363b87bcc48edfdc4c720ae92251e
dev-vming <dev.vming@gmail.com>
2025-12-19 00:10:04 +0900

    feat: 세션에서 Id 값 가지고 있도록 수정 및 타입 정의

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,44 +1,62 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/db";
 
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 
 import GoogleProvider from "next-auth/providers/google";
 import NaverProvider from "next-auth/providers/naver";
 import KakaoProvider from "next-auth/providers/kakao";
 
-export const authOptions = {
-  adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: 'jwt' as const,
-    maxAge: 60 * 60 * 24,
-    updateAge: 60 * 60 * 2,
-  },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    }),
-    NaverProvider({
-      clientId: process.env.NAVER_CLIENT_ID || "",
-      clientSecret: process.env.NAVER_CLIENT_SECRET || "",
-      profile(profile) {
-        return {
-          id: profile.response.id,
-          name: profile.response.name || profile.response.nickname || null,
-          email: profile.response.email,
-          image: profile.response.profile_image,
-        };
-      },
-    }),
-    KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID || "",
-      clientSecret: process.env.KAKAO_CLIENT_SECRET || "",
-    }),
-  ],
-  pages: {
-    signIn: '/users/login',
-  }
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    session: {
+        strategy: "jwt" as const,
+        maxAge: 60 * 60 * 24,
+        updateAge: 60 * 60 * 2,
+    },
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+        }),
+        NaverProvider({
+            clientId: process.env.NAVER_CLIENT_ID || "",
+            clientSecret: process.env.NAVER_CLIENT_SECRET || "",
+            profile(profile) {
+                return {
+                    id: profile.response.id,
+                    name:
+                        profile.response.name ||
+                        profile.response.nickname ||
+                        null,
+                    email: profile.response.email,
+                    image: profile.response.profile_image,
+                };
+            },
+        }),
+        KakaoProvider({
+            clientId: process.env.KAKAO_CLIENT_ID || "",
+            clientSecret: process.env.KAKAO_CLIENT_SECRET || "",
+        }),
+    ],
+    pages: {
+        signIn: "/users/login",
+    },
+    callbacks: {
+        session: ({ session, token }) => ({
+            ...session,
+            user: {
+                ...session.user,
+                id: token.sub,
+            },
+        }),
+        jwt: async ({ user, token }) => {
+            if (user) {
+                token.sub = user.id;
+            }
+            return token;
+        },
+    },
 };
 
 export default NextAuth(authOptions);
