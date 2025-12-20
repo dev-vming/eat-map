cf0866aa3138bfbcb57d8c99e64d54b3b89f0ea9
dev-vming <dev.vming@gmail.com>
2025-12-20 22:30:42 +0900

    feat: 찜한 맛집 페이지 리스트 작업

--- src/components/StoreList.tsx
+++ src/components/StoreList.tsx
@@ -3,22 +3,22 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 
 interface StoreListProps {
-    store: StoreType;
+    store?: StoreType;
     index: number;
 }
 
-export default function StoreList({store, index}: StoreListProps) {
+export default function StoreList({ store, index }: StoreListProps) {
     const router = useRouter();
     return (
         <li
-            onClick={() => router.push(`/stores/${store.id}`)}
+            onClick={() => router.push(`/stores/${store?.id}`)}
             className="flex justify-between gap-x-6 py-5 cursor-pointer hover:bg-gray-50"
             key={index}
         >
             <div className="flex gap-x-4">
                 <Image
                     src={
-                        store.category
+                        store?.category
                             ? `/images/markers/${store.category}.png`
                             : "/images/markers/default.png"
                     }
@@ -28,22 +28,22 @@ export default function StoreList({store, index}: StoreListProps) {
                 />
                 <div>
                     <div className="text-sm font-semibold leading-6 text-gray-900">
-                        {store.name}
+                        {store?.name}
                     </div>
                     <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
-                        {store.storeType}
+                        {store?.storeType}
                     </div>
                 </div>
             </div>
             <div className="hidden sm:flex sm:flex-col sm:items-end">
                 <div className="text-sm font-semibold leading-6 text-gray-900">
-                    {store.address}
+                    {store?.address}
                 </div>
                 <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
-                    {store.phone || "번호없음"} | {store.foodCertifyName} |{" "}
-                    {store.category === "default"
+                    {store?.phone || "번호없음"} | {store?.foodCertifyName} |{" "}
+                    {store?.category === "default"
                         ? "카테고리없음"
-                        : store.category}
+                        : store?.category}
                 </div>
             </div>
         </li>
