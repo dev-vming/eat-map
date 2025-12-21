fa6efe796b1003b58165fd8809c8dcde80aaae6f
dev-vming <dev.vming@gmail.com>
2025-12-22 08:57:03 +0900

    feat: 댓글 작성 폼 컴포넌트 분리 및 api 연동

--- src/components/comments/index.tsx
+++ src/components/comments/index.tsx
@@ -1,45 +1,18 @@
-import { useForm } from "react-hook-form";
+import { useSession } from "next-auth/react";
+import CommentForm from "./CommentForm";
 
 interface CommentProps {
     storeId: number;
 }
 
 export default function Comments({ storeId }: CommentProps) {
-    const {
-        register,
-        handleSubmit,
-        resetField,
-        formState: { errors },
-    } = useForm();
+    const { status } = useSession();
+
     return (
         <div className="px-2 md:max-w-4xl mx-auto py-8 mb-20">
-            {/* comment form */}
-
-            <form
-                onSubmit={handleSubmit(async (data) => {
-                    console.log(data);
-                })}
-                className="flex flex-col space-y-2"
-            >
-                {errors.body?.type === "required" && (
-                    <div className="text-xs text-red-600">
-                        필수 입력 사항입니다.
-                    </div>
-                )}
-                <textarea
-                    placeholder="댓글을 작성해주세요..."
-                    {...register("body", { required: true })}
-                    className="block w-full min-h-[120px] resize-none border rounded-md bg-transparent py-2.5 px-4 text-black placeholder:text-gray-400 text-sm leading-6"
-                    rows={3}
-                />
-
-                <button
-                    type="submit"
-                    className="bg-blue-600 hover:bg-blue-500 text-white px-4 py-2 text-sm font-semibold shadow-sm rounded-md"
-                >
-                    작성하기
-                </button>
-            </form>
+            {/* 댓글 작성 폼 */}
+            {status === "authenticated" && <CommentForm storeId={storeId} />}
+            {/* 댓글 리스트 */}
         </div>
     );
 }
