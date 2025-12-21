f04cb358d2bfea049543462253f4d237c4bffb04
dev-vming <dev.vming@gmail.com>
2025-12-22 08:56:40 +0900

    feat: 댓글 작성 (POST) api 작업

---
+++ src/pages/api/comments.ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
+import prisma from "@/db";
+import { CommentApiResponse, CommentInterface } from "@/interface";
+
+interface ResponseType {
+    page?: string;
+    limit?: string;
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<CommentInterface | CommentApiResponse>
+) {
+    const session = await getServerSession(req, res, authOptions);
+
+    if (req.method === "POST") {
+        // 댓글 작성 로직
+        if (!session?.user) {
+            return res.status(401);
+        }
+
+        const { storeId, body }: { storeId: number; body: string } = req.body;
+        const comment = await prisma.comment.create({
+            data: {
+                storeId,
+                body,
+                userId: parseInt(session?.user.id),
+            },
+        });
+        return res.status(200).json(comment);
+    } else if (req.method === "DELETE") {
+        // 댓글 삭제 로직
+    } else {
+        // 댓글 가져오기 (GET 요청 처리)
+    }
+}
